bce6a56fd143f26405f03d7acc6a256c7aae1282
Mukul jangir <64830777+Mukuljangir372@users.noreply.github.com>
2022-03-06 10:33:49 +0530

    Update cache.kt

--- cache.kt
+++ cache.kt
@@ -1,4 +1,29 @@
 
+
+/**
+* Why SupervisorJob ?
+* Why Coroutines?
+* You and me, already know, how kotlin coroutines efficient way to handle back or main threads.
+* Basically, Rather than using Job(), SupervisorJob() better handles the Throwables or errors.
+* and don't cancel automatically when there any error occurs in network side or db queries.
+* 
+* Why inline or crossline? 
+* Ok, That's two keywords from kotlin. They Inline simply prevents the function object creation everytime we call.
+* and crossline used with lambadas, They prevents the return value from inner called lambadas. Here we are using flow, So
+* we don't want that anyone will return anything without our permission.
+*
+* Why suspend? 
+* Offcourse, we use it with kotlin coroutines that suspend the ui.
+* 
+* Why not genric type? As we are using smart cast?
+* Ok, Generic type and smart cast jobs are same. But handling the casting is better done by smart cast.
+* 
+* 
+* SIDE EFFECTS -> 
+* upcoming...
+* 
+*/
+
 object Cache {
     var defaultScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
 
