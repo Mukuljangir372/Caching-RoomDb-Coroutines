7e5ce02c22ee4443f102d045b9021148dfefda77
Mukul jangir <64830777+Mukuljangir372@users.noreply.github.com>
2022-03-06 10:22:50 +0530

    Create Usage.kt

---
+++ Usage.kt
@@ -0,0 +1,37 @@
+  
+
+/**
+* 
+* How to use cache network call?
+*
+*/
+suspend fun viewTicket2(ticketId: Int) = cacheNetwork(
+       localQuery = {
+           ticketDao.getViewTicketData(ticketId) //return type flow
+       },
+       networkFetch = {
+           repository.viewTickets(ticketId) //return type Resource<*>
+       },
+       lastQueryTime = {
+           it.lastFetchedTime //return type long 
+       },
+       saveNetworkResult = {
+           it.data!!.data.lastFetchedTime = System.currentTimeMillis()
+           ticketDao.insertViewTicketData(it.data.data)
+           it.data.data //return the actual data you collected here
+       }
+   )
+
+
+/**
+* Colleting cache data 
+*/
+
+class Usage{
+  fun getUsers() {
+     viewModel.viewTicket2(ticketId).collectLatest{
+       //collect here
+       //sometimes null collection when network calls and db both fails to get data
+     }
+  }
+}
